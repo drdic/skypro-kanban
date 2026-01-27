e2d8bc78ecf71ed42bf376ba630ad3e8985e1952
drdic <drdic26@gmail.com>
2026-01-27 19:40:53 +0300

    Возвращаем старый хедер

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper">
     <!-- Временно используем упрощенный заголовок -->
-    <BaseHeaderSimple />
+    <BaseHeader />
     <main class="main">
       <TaskDesk />
     </main>
@@ -17,13 +17,13 @@ import TaskDesk from '@/components/TaskDesk.vue'
 import TaskModal from '@/components/TaskModal.vue'
 import NewCardModal from '@/components/NewCardModal.vue'
 import ExitModal from '@/components/ExitModal.vue'
-import BaseHeaderSimple from '@/components/BaseHeaderSimple.vue'
+//import BaseHeaderSimple from '@/components/BaseHeaderSimple.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    BaseHeaderSimple,
-    // BaseHeader,
+    //BaseHeaderSimple,
+    BaseHeader,
     TaskDesk,
     TaskModal,
     NewCardModal,

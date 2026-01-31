d490fcac74592a6ba48741dedb2145619598e88b
drdic <drdic26@gmail.com>
2026-01-31 16:54:51 +0300

    Добавил scoped стили в TaskColumn

--- src/components/TaskColumn.vue
+++ src/components/TaskColumn.vue
@@ -15,3 +15,31 @@ export default {
   props: ['title'],
 }
 </script>
+
+<style scoped>
+/* Добавляем scoped стили */
+.main__column {
+  width: 20%;
+  margin: 0 auto;
+  display: block;
+}
+
+.column__title {
+  padding: 0 10px;
+  margin: 15px 0;
+}
+
+.column__title p {
+  color: v-bind('theme.colors.textMuted');
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 1;
+  text-transform: uppercase;
+}
+
+.cards {
+  width: 100%;
+  display: block;
+  position: relative;
+}
+</style>

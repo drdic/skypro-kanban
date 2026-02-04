7a9d72214c76d721d90c74ed9f1398bb3117d2cf
drdic <drdic26@gmail.com>
2026-02-04 23:54:25 +0300

    Заменены ссылки на router-link в BaseHeader

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -3,25 +3,30 @@
     <div class="container">
       <div class="header__block">
         <div class="header__logo _show _light">
-          <a href="#" target="_self"><img src="@/assets/images/logo.png" alt="logo" /></a>
+          <router-link to="/">
+            <img src="@/assets/images/logo.png" alt="logo" />
+          </router-link>
         </div>
         <div class="header__logo _dark">
-          <a href="#" target="_self"><img src="@/assets/images/logo_dark.png" alt="logo" /></a>
+          <router-link to="/">
+            <img src="@/assets/images/logo_dark.png" alt="logo" />
+          </router-link>
         </div>
         <nav class="header__nav">
-          <button class="header__btn-main-new _hover01" id="btnMainNew">
-            <a href="#popNewCard">Создать новую задачу</a>
+          <button class="header__btn-main-new _hover01">
+            <router-link to="/add">Создать новую задачу</router-link>
           </button>
-          <a href="#user-set-target" class="header__user _hover02">Ivan Ivanov</a>
+          <router-link to="/" class="header__user _hover02">Ivan Ivanov</router-link>
           <div class="header__pop-user-set pop-user-set" id="user-set-target">
-            <!-- <a href="">x</a> -->
             <p class="pop-user-set__name">Ivan Ivanov</p>
             <p class="pop-user-set__mail">ivan.ivanov@gmail.com</p>
             <div class="pop-user-set__theme">
               <p>Темная тема</p>
               <input type="checkbox" class="checkbox" name="checkbox" />
             </div>
-            <button type="button" class="_hover03"><a href="#popExit">Выйти</a></button>
+            <button type="button" class="_hover03">
+              <router-link to="/exit">Выйти</router-link>
+            </button>
           </div>
         </nav>
       </div>

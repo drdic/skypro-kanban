9ff60fcc9e01724fd9eeaf39ab36e6d39baee779
drdic <drdic26@gmail.com>
2026-02-20 16:51:53 +0300

    Удалил модальные окна и ссылки

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,27 +4,18 @@
     <main class="main">
       <TaskDesk />
     </main>
-    <TaskModal />
-    <NewCardModal />
-    <ExitModal />
   </div>
 </template>
 
 <script>
 import BaseHeader from '@/components/BaseHeader.vue'
 import TaskDesk from '@/components/TaskDesk.vue'
-import TaskModal from '@/components/TaskModal.vue'
-import NewCardModal from '@/components/NewCardModal.vue'
-import ExitModal from '@/components/ExitModal.vue'
 
 export default {
   name: 'HomeView',
   components: {
     BaseHeader,
     TaskDesk,
-    TaskModal,
-    NewCardModal,
-    ExitModal,
   },
 }
 </script>

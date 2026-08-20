9760735ca02908a5d3a2285304cb899b0f68ff2b
Eduard <drdic26@gmail.com>
2026-08-20 23:40:07 +0300

    Реализовал открытие/закрытие попапа пользователя по клику

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -8,19 +8,21 @@
         <div class="header__logo _dark">
           <a href="#" target="_self"><img src="/assets/images/logo_dark.png" alt="logo" /></a>
         </div>
-        <nav class="header__nav">
+        <nav class="header__nav" ref="headerNav">
           <button class="header__btn-main-new _hover01" id="btnMainNew">
             <a href="#popNewCard">Создать новую задачу</a>
           </button>
-          <a href="#user-set-target" class="header__user _hover02">Ivan Ivanov</a>
-          <div class="header__pop-user-set pop-user-set" id="user-set-target">
+          <a href="#" class="header__user _hover02" @click.prevent="togglePopup">Ivan Ivanov</a>
+          <div v-if="isPopupOpen" class="header__pop-user-set pop-user-set">
             <p class="pop-user-set__name">Ivan Ivanov</p>
             <p class="pop-user-set__mail">ivan.ivanov@gmail.com</p>
             <div class="pop-user-set__theme">
               <p>Темная тема</p>
               <input type="checkbox" class="checkbox" name="checkbox" />
             </div>
-            <button type="button" class="_hover03"><a href="#popExit">Выйти</a></button>
+            <button type="button" class="_hover03" @click="closePopup">
+              <a href="#popExit" @click="closePopup">Выйти</a>
+            </button>
           </div>
         </nav>
       </div>
@@ -29,7 +31,42 @@
 </template>
 
 <script>
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+
 export default {
   name: 'BaseHeader',
+  setup() {
+    const isPopupOpen = ref(false)
+    const headerNav = ref(null)
+
+    const togglePopup = () => {
+      isPopupOpen.value = !isPopupOpen.value
+    }
+
+    const closePopup = () => {
+      isPopupOpen.value = false
+    }
+
+    const handleClickOutside = (event) => {
+      if (headerNav.value && !headerNav.value.contains(event.target)) {
+        closePopup()
+      }
+    }
+
+    onMounted(() => {
+      document.addEventListener('click', handleClickOutside)
+    })
+
+    onBeforeUnmount(() => {
+      document.removeEventListener('click', handleClickOutside)
+    })
+
+    return {
+      isPopupOpen,
+      headerNav,
+      togglePopup,
+      closePopup,
+    }
+  },
 }
 </script>

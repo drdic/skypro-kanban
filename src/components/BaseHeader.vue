1771ddb9010d380cd8d21d1a1a7bd477303d219c
Eduard <drdic26@gmail.com>
2026-08-25 23:10:36 +0300

    Вынес цвета в отдельный файл палитры и добавил стили шапке, карточкам и колонкам. Заодно сделал анимацию загрузки и надпись «Задач нет», если карточек нет.

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -70,3 +70,178 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.header {
+  width: 100%;
+  margin: 0 auto;
+  background-color: var(--color-bg-white);
+}
+
+.header__block {
+  height: 70px;
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: center;
+  justify-content: space-between;
+  position: relative;
+  top: 0;
+  left: 0;
+  padding: 0 10px;
+}
+
+.header__logo img {
+  width: 85px;
+}
+
+.header__nav {
+  max-width: 290px;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.header__btn-main-new {
+  width: 178px;
+  height: 30px;
+  border-radius: 4px;
+  background-color: var(--color-accent);
+  color: var(--color-text-white);
+  border: none;
+  font-size: 14px;
+  line-height: 1;
+  font-weight: 500;
+  margin-right: 20px;
+}
+
+.header__btn-main-new a {
+  color: var(--color-text-white);
+}
+
+.header__user {
+  height: 20px;
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  line-height: 20px;
+  color: var(--color-accent);
+}
+
+.header__user::after {
+  content: "";
+  display: block;
+  width: 6px;
+  height: 6px;
+  border-radius: 1px;
+  border-left: 1.9px solid var(--color-accent);
+  border-bottom: 1.9px solid var(--color-accent);
+  transform: rotate(-45deg);
+  margin: -6px 0 0 5px;
+  padding: 0;
+}
+
+.header__pop-user-set {
+  display: block;
+  position: absolute;
+  top: 61px;
+  right: 0;
+  width: 213px;
+  height: 205px;
+  border-radius: 10px;
+  border: 0.7px solid var(--color-border-light);
+  background: var(--color-bg-white);
+  box-shadow: 0px 10px 39px 0px var(--color-shadow-popup);
+  padding: 34px;
+  text-align: center;
+  z-index: 2;
+}
+
+.pop-user-set__name {
+  color: var(--color-text-primary);
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 21px;
+  letter-spacing: -0.14px;
+  margin-bottom: 4px;
+}
+
+.pop-user-set__mail {
+  color: var(--color-text-secondary);
+  font-size: 14px;
+  line-height: 21px;
+  letter-spacing: -0.14px;
+  margin-bottom: 10px;
+}
+
+.pop-user-set__theme {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 30px;
+}
+
+.pop-user-set__theme p {
+  color: var(--color-text-primary);
+  font-size: 14px;
+  line-height: 21px;
+  letter-spacing: -0.14px;
+}
+
+.pop-user-set__theme input[type="checkbox"] {
+  position: relative;
+  width: 24px;
+  height: 13px;
+  border-radius: 100px;
+  background: var(--color-bg-content);
+  outline: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+}
+
+.pop-user-set__theme input[type="checkbox"]::before {
+  content: "";
+  position: absolute;
+  top: 1px;
+  left: 1px;
+  width: 11px;
+  height: 11px;
+  border-radius: 50%;
+  background-color: var(--color-text-secondary);
+  transition: 0.5s;
+}
+
+.pop-user-set__theme input:checked[type="checkbox"]::before {
+  left: 12px;
+}
+
+.pop-user-set button {
+  width: 72px;
+  height: 30px;
+  background: transparent;
+  color: var(--color-accent);
+  border-radius: 4px;
+  border: 1px solid var(--color-accent);
+}
+
+.pop-user-set button a {
+  color: var(--color-accent);
+}
+
+@media screen and (max-width: 495px) {
+  .header__btn-main-new {
+    z-index: 3;
+    position: fixed;
+    left: 16px;
+    bottom: 30px;
+    top: auto;
+    width: calc(100vw - 32px);
+    height: 40px;
+    border-radius: 4px;
+    margin-right: 0;
+  }
+}
+</style>
